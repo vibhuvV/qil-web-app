1c0a4013865fde1bd6c0addf4bdf67da431d7c39
Vaibhav Verma <vaibhavvermavibhu@gmail.com>
2025-07-13 19:14:04 +0530

    chore(env): added environment variable for landing page

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -2,3 +2,4 @@ export const BASE_APP_LOCAL_STORAGE_KEY =
   import.meta.env.VITE_APP_LS_KEY ?? "test-maker";
 export const BASE_APP_URL = import.meta.env.VITE_BASE_API_URL;
 export const S3_BASE_URL = import.meta.env.VITE_S3_BASE_URL;
+export const BASE_LANDING_PAGE_URL = import.meta.env.VITE_BASE_LANDING_PAGE_URL;
--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_APP_LS_KEY: string;
   readonly VITE_BASE_API_URL: string;
   readonly VITE_S3_BASE_URL: string;
+  readonly VITE_BASE_LANDING_PAGE_URL: string;
 }
 
 interface ImportMeta {
